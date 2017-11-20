071b02083841287a810210b9085e092256d60815
Simonppg <simonppg@gmail.com>
2017-11-19 19:50:48 -0600

    First triangle

--- app/src/main/cpp/game.c
+++ app/src/main/cpp/game.c
@@ -2,17 +2,136 @@
 // Created by Simonppg on 11/4/2017.
 //
 
+#include <GLES2/gl2.h>
+#include <stdbool.h>
+
 #include "game.h"
-#include "glwrapper.h"
+
+#include <jni.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char glVertexShader[] =
+        "attribute vec4 vPosition;\n"
+        "void main()\n"
+        "{\n"
+        "  gl_Position = vPosition;\n"
+        "}\n";
+
+static const char glFragmentShader[] =
+        "precision mediump float;\n"
+        "void main()\n"
+        "{\n"
+        "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+GLuint loadShader(GLenum shaderType, const char* shaderSource)
+{
+    GLuint shader = glCreateShader(shaderType);
+    if (shader)
+    {
+        glShaderSource(shader, 1, &shaderSource, NULL);
+        glCompileShader(shader);
+        GLint compiled = 0;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if (!compiled)
+        {
+            GLint infoLen = 0;
+            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+            if (infoLen)
+            {
+                char * buf = (char*) malloc(infoLen);
+                if (buf)
+                {
+                    glGetShaderInfoLog(shader, infoLen, NULL, buf);
+                    free(buf);
+                }
+                glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+    }
+    return shader;
+}
+
+GLuint createProgram(const char* vertexSource, const char * fragmentSource)
+{
+    GLuint vertexShader = loadShader(GL_VERTEX_SHADER, vertexSource);
+    if (!vertexShader)
+    {
+        return 0;
+    }
+    GLuint fragmentShader = loadShader(GL_FRAGMENT_SHADER, fragmentSource);
+    if (!fragmentShader)
+    {
+        return 0;
+    }
+    GLuint program = glCreateProgram();
+    if (program)
+    {
+        glAttachShader(program , vertexShader);
+        glAttachShader(program, fragmentShader);
+        glLinkProgram(program);
+        GLint linkStatus = GL_FALSE;
+        glGetProgramiv(program , GL_LINK_STATUS, &linkStatus);
+        if( linkStatus != GL_TRUE)
+        {
+            GLint bufLength = 0;
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
+            if (bufLength)
+            {
+                char* buf = (char*) malloc(bufLength);
+                if (buf)
+                {
+                    glGetProgramInfoLog(program, bufLength, NULL, buf);
+                    free(buf);
+                }
+            }
+            glDeleteProgram(program);
+            program = 0;
+        }
+    }
+    return program;
+}
+
+GLuint simpleTriangleProgram;
+GLuint vPosition;
+bool setupGraphics(int w, int h)
+{
+    simpleTriangleProgram = createProgram(glVertexShader, glFragmentShader);
+    if (!simpleTriangleProgram)
+    {
+        return false;
+    }
+    vPosition = glGetAttribLocation(simpleTriangleProgram, "vPosition");
+    glViewport(0, 0, w, h);
+    return true;
+}
+
+const GLfloat triangleVertices[] = {
+        0.0f, 1.0f,
+        -1.0f, -1.0f,
+        1.0f, -1.0f
+};
+void renderFrame()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear (GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    glUseProgram(simpleTriangleProgram);
+    glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0 ,triangleVertices);
+    glEnableVertexAttribArray(vPosition);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
 
 void on_surface_created() {
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    setupGraphics(800, 600);
 }
 
 void on_surface_changed() {
-    // No-op
 }
 
 void on_draw_frame() {
     glClear(GL_COLOR_BUFFER_BIT);
+    renderFrame();
 }
