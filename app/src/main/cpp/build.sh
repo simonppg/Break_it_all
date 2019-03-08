24030ad60629eb7bb26ef2b7929354429e752116
simonppg <simonppg@gmail.com>
2019-03-08 12:53:56 -0700

    Adding simple OpenGL examples

---
+++ app/src/main/cpp/build.sh
@@ -0,0 +1,3 @@
+g++ linux/main.cpp linux/logger.cpp game.cpp linux/filesManager.cpp triangle.cpp utils.cpp -DPROJECT_DIR=\"/home/simonppg/break_it_all/app\" -lGLESv2 -lglfw -o es.out
+
+g++ linux/main.cpp linux/logger.cpp game.cpp linux/filesManager.cpp triangle.cpp utils.cpp -DPROJECT_DIR=\"/home/simonppg/break_it_all/app\" -lGL -lglfw -o ew.out
---
+++ app/src/main/cpp/examples/window/build.sh
@@ -0,0 +1,2 @@
+g++ window.cpp -lglfw -lGLESv2 -o es2.out
+g++ window.cpp -lglfw -lGL -o gl.out
