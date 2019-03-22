85025cb1627525c802fbfeee42b8b2dfdfb6e0db
simonppg <simonppg@gmail.com>
2019-03-22 16:13:28 -0700

    Updating linux build scripts

--- app/src/main/cpp/build.sh
+++ app/src/main/cpp/build.sh
@@ -1,3 +1,3 @@
-g++ linux/main.cpp linux/logger.cpp game.cpp linux/filesManager.cpp triangle.cpp utils.cpp -DPROJECT_DIR=\"/home/simonppg/break_it_all/app\" -lGLESv2 -lglfw -o es.out
+g++ linux/main.cpp game.cpp linux/filesManager.cpp triangle.cpp utils.cpp Cube.cpp Camera.cpp square.cpp -DPROJECT_DIR=\"${HOME}/break_it_all/app\" -lGLESv2 -lglfw -o es.out
 
-g++ linux/main.cpp linux/logger.cpp game.cpp linux/filesManager.cpp triangle.cpp utils.cpp -DPROJECT_DIR=\"/home/simonppg/break_it_all/app\" -lGL -lglfw -o ew.out
+g++ linux/main.cpp game.cpp linux/filesManager.cpp triangle.cpp utils.cpp Cube.cpp Camera.cpp square.cpp -DPROJECT_DIR=\"${HOME}/break_it_all/app\" -lGL -lglfw -o gl.out
