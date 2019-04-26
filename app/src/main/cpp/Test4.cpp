dde29556160a25486fbc909a6e5cdda913e1ee0c
simonppg <simonppg@gmail.com>
2019-04-26 15:07:46 -0700

    Animate the player paddle

--- app/src/main/cpp/Test4.cpp
+++ app/src/main/cpp/Test4.cpp
@@ -10,6 +10,15 @@ static float pov_in_degrees = 0.0f;
 #define NUMBER_OF_VERTICES 15
 static float radius = 1.0;
 
+static float h = (float)HEIGHT/2;
+static vector<float> vPos = Math::get_grid(WIDTH, h, ROW, COL);
+static float x_size = 90.0f*((float)WIDTH/COL)/100;
+static float y_size = 80.0f*(h/ROW)/100;
+
+static float ball_size = (float)WIDTH/30;
+static float cube_x_size = ((float)WIDTH/2)/3;
+static float cube_y_size = cube_x_size/7;
+
 Test4::Test4() {
     camera = new Camera(WIDTH, HEIGHT, X, Y, Z, NCP, FCP, FOV);
     camera->set_projection_type(ORTHO);
@@ -20,20 +29,20 @@ Test4::Test4() {
     meshes[0] = new Mesh(Math::get_cube(), 16, Math::get_cube_index(), 36);
     meshes[1] = new Mesh(Math::get_circle(radius, NUMBER_OF_VERTICES), NUMBER_OF_VERTICES);
 
-    for (auto &i : objects) {
-        i = new Object(camera, shaderProgs[1], meshes[0]);
-        i->update_size(2, 1, 1);
-        i->animate_y();
+    for (int i = 0; i < ROW * COL; i++) {
+        objects[i] = new Object(camera, shaderProgs[1], meshes[0]);
+        objects[i]->update_size(x_size/2, y_size/2, 1);
+        objects[i]->update_xyz(vPos[i * 2], vPos[i * 2 + 1], 0);
     }
 
     ball = new Object(camera, shaderProgs[1], meshes[1]);
-    ball->update_size(15, 15, 1);
-    ball->update_xyz(0, 0, 0);
+    ball->update_size(ball_size, ball_size, 1);
+    ball->update_xyz(0, -h + h/3, 0);
     ball->animate_x();
 
     paddle = new Object(camera, shaderProgs[1], meshes[0]);
-    paddle->update_size(40, 10, 1);
-    paddle->update_xyz(0, 0, 0);
+    paddle->update_size(cube_x_size, cube_y_size, 1);
+    paddle->update_xyz(0, -h + cube_y_size*2, 1);
     paddle->animate_y();
 }
 
@@ -68,35 +77,40 @@ void Test4::surfaceChanged(int width, int height) {
     glViewport(0, 0, width, height);
     camera->update_width_height(width, height);
 
-    float h = (float)height/2;
-    vector<float> vPos = Math::get_grid(width, h, ROW, COL);
-    float x_size = 90.0f*((float)width/COL)/100;
-    float y_size = 80.0f*(h/ROW)/100;
+    h = (float)height/2;
+    vPos = Math::get_grid(width, h, ROW, COL);
+    x_size = 90.0f*((float)width/COL)/100;
+    y_size = 80.0f*(h/ROW)/100;
 
     for (int i = 0; i < ROW * COL; i++) {
         objects[i]->update_size(x_size/2, y_size/2, 1);
         objects[i]->update_xyz(vPos[i * 2], vPos[i * 2 + 1], 0);
     }
 
-    float ball_size = (float)width/30;
+    ball_size = (float)width/30;
+    cube_x_size = ((float)width/2)/3;
+    cube_y_size = cube_x_size/7;
+
     ball->update_size(ball_size, ball_size, 1);
     ball->update_xyz(0, -h + h/3, 0);
 
-    float cube_x_size = ((float)width/2)/3;
-    float cube_y_size = cube_x_size/7;
     paddle->update_size(cube_x_size, cube_y_size, 1);
     paddle->update_xyz(0, -h + cube_y_size*2, 1);
 }
 
 void Test4::update() {
-    for(auto &i : objects) {
+    /*for(auto &i : objects) {
         i->set_rotation_angle(pov_in_degrees);
-    }
-    ball->set_rotation_angle(pov_in_degrees);
-    paddle->set_rotation_angle(pov_in_degrees);
+    }*/
+    //ball->set_rotation_angle(pov_in_degrees);
+    //paddle->set_rotation_angle(pov_in_degrees);
 }
 
 bool Test4::events(double xpos, double ypos) {
+    LOGI("%.2f, %.2f", xpos, ypos);
+
+    //ball->update_xyz((-camera->w/2) + xpos, (camera->h / 2) - ypos, 0);
+    paddle->update_xyz((-camera->w/2) + xpos, (-camera->h/2) + cube_y_size*2, 0);
     pov_in_degrees += 5.0f;
     return true;
 }
