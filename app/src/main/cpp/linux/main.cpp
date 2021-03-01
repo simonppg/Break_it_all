55b451f1e2d82bd97363b54bcf59fd131b1a05ea
simonppg <simonppg@gmail.com>
2021-02-28 21:15:10 -0700

    Open a window in the new linux version

--- app/src/main/cpp/linux/main.cpp
+++ app/src/main/cpp/linux/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include "BreakItAll.hpp"
+#include "WindowManager.hpp"
 
 int main() {
     BreakItAll *p = new BreakItAll();
 
     p->init();
 
+    int width = 450, height = 800;
+
+    WindowManager *wm = new WindowManager();
+
+    if(wm->createWindow(width, height) != 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    while (!wm->shouldClose()) {
+        wm->pollEvents();
+        
+        wm->refreshWindow();
+    }
+
+    wm->destroyWindow();
     std::cout << std::endl << "Hello World!" << std::endl << std::endl;
     return 0;
 }
+
