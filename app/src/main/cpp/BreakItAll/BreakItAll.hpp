29e69adc83566e7bb0568260994a8801c10d3847
simonppg <simonppg@gmail.com>
2021-02-28 20:03:44 -0700

    Make branch compilation by platform on top CMakeLists.txt file

---
+++ app/src/main/cpp/BreakItAll/BreakItAll.hpp
@@ -0,0 +1,9 @@
+#ifndef _BREAK_IT_ALL_H_
+#define _BREAK_IT_ALL_H_
+
+class BreakItAll {
+public:
+    void init();
+};
+
+#endif //_BREAK_IT_ALL_H_
--- app/src/main/cpp/android/main.cpp
+++ app/src/main/cpp/android/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "BreakItAll.hpp"
 
 int main() {
     std::cout << "Hello World!";
