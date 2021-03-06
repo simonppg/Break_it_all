76d024d69ed7b12803fb0c0b13fb9dacf6f03b5b
simonppg <simonppg@gmail.com>
2021-03-06 10:11:03 -0700

    Refactor BreakItAllActivity.java extracting some methods, now it can read position from intent extras

--- app/src/main/java/com/example/simonppg/break_it_all/BreakItAllActivity.java
+++ app/src/main/java/com/example/simonppg/break_it_all/BreakItAllActivity.java
@@ -1,6 +1,8 @@
 package com.example.simonppg.break_it_all;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -16,31 +18,57 @@ public class BreakItAllActivity extends Activity {
     @Override protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-            mView = new GLSurfaceView(getApplication());
-            mView.setEGLContextClientVersion(2);
-            mView.setRenderer(new GLSurfaceView.Renderer() {
-                @Override
-                public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-                    BreakItAllLibJNIWrapper.surfaceCreated();
-                }
+        final int position = getPosition(getIntent());
+        showPosition(position, getApplicationContext());
 
-                @Override
-                public void onSurfaceChanged(GL10 gl, int width, int height) {
-                    BreakItAllLibJNIWrapper.surfaceChanged(width, height);
-                }
-
-                @Override
-                public void onDrawFrame(GL10 gl) {
-                    BreakItAllLibJNIWrapper.drawFrame();
-                }
-            });
-            mView.queueEvent(new Runnable() {
+        mView = new GLSurfaceView(getApplication());
+        mView.setEGLContextClientVersion(2);
+        mView.setRenderer(getRenderer());
+        mView.queueEvent(new Runnable() {
                 @Override
                 public void run() {
-                    BreakItAllLibJNIWrapper.init(0, getAssets());
+                    BreakItAllLibJNIWrapper.init(position, getAssets());
                 }
             });
-            setContentView(mView);
+        setContentView(mView);
+    }
+
+    private GLSurfaceView.Renderer getRenderer() {
+        return new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+                BreakItAllLibJNIWrapper.surfaceCreated();
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+                BreakItAllLibJNIWrapper.surfaceChanged(width, height);
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+                BreakItAllLibJNIWrapper.drawFrame();
+            }
+        };
+    }
+
+    private void showPosition(int position, Context context) {
+        Toast.makeText(context,
+                    "Click ListItem Number " + position, Toast.LENGTH_SHORT)
+                    .show();
+    }
+
+    private int getPosition(Intent intent) {
+        Bundle extras;
+        int defaultPosition = 0;
+
+        if(intent == null) { return defaultPosition; }
+
+        extras = intent.getExtras();
+
+        if(extras == null) { return defaultPosition; }
+
+        return extras.getInt("position");
     }
 
     @Override protected void onPause() {
