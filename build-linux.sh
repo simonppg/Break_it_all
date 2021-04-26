a9a54fdf9dd5f12974da3a0e07421a78484ad257
simonppg <simonppg@gmail.com>
2021-04-25 19:21:05 -0700

    Build glfw directly in CMake

---
+++ build-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BUILD_DIR=linux-build
+
+# Delete folder if exist
+if [ -d "$BUILD_DIR" ]; then rm -Rf $BUILD_DIR; fi
+
+# Create folder, move inside, build and run
+mkdir -pv $BUILD_DIR && cd $BUILD_DIR && cmake .. && make runNew
