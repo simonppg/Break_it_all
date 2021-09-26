9520648e0061d85df38ac31a76ff1dcb6b1a8802
simonppg <simonppg@gmail.com>
2021-09-25 18:31:52 -0700

    Update include guard

--- app/src/main/cpp/linux/LinuxFilesManager.hpp
+++ app/src/main/cpp/linux/LinuxFilesManager.hpp
@@ -1,5 +1,5 @@
-#ifndef _FILES_MANAGER_H_
-#define _FILES_MANAGER_H_
+#ifndef _LINUX_FILES_MANAGER_H_
+#define _LINUX_FILES_MANAGER_H_
 
 #include "../shared/FilesManager.hpp"
 
@@ -8,4 +8,4 @@ public:
     char* loadFile(const char *filePath);
 };
 
-#endif //_FILES_MANAGER_H_
+#endif //_LINUX_FILES_MANAGER_H_
