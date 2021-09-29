bcd9d69771ff6c78677852b81ba4f1c7e7a882ee
simonppg <simonppg@gmail.com>
2021-09-29 13:46:36 -0700

    Format code

--- app/src/main/cpp/common/ShaderLoader.cpp
+++ app/src/main/cpp/common/ShaderLoader.cpp
@@ -2,44 +2,44 @@
 #include <malloc.h>
 
 bool ShaderLoader::isCompilationOk(GLenum shader) {
-    GLint compiled = 0;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-    return compiled;
+  GLint compiled = 0;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+  return compiled;
 }
 
 int ShaderLoader::getInfoLogLength(GLenum shader) {
-    GLint infoLen = 0;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-    return infoLen;
+  GLint infoLen = 0;
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+  return infoLen;
 }
 
 void ShaderLoader::showShaderInfoLog(GLenum shader) {
-    int infoLength = getInfoLogLength(shader);
-    if (!infoLength) { return; }
+  int infoLength = getInfoLogLength(shader);
+  if (!infoLength) { return; }
 
-    char *buf = (char*) malloc(sizeof(char) * infoLength);
-    if (!buf) { return; }
+  char *buf = (char *)malloc(sizeof(char) * infoLength);
+  if (!buf) { return; }
 
-    glGetShaderInfoLog(shader, infoLength, NULL, buf);
-    // LOGE("%s", buf);
-    free(buf);
+  glGetShaderInfoLog(shader, infoLength, NULL, buf);
+  // LOGE("%s", buf);
+  free(buf);
 }
 
-GLuint ShaderLoader::loadShader(GLenum shaderType, const char* shaderSource) {
-    // In this function 0 is and error
-    int error = 0;
+GLuint ShaderLoader::loadShader(GLenum shaderType, const char *shaderSource) {
+  // In this function 0 is and error
+  int error = 0;
 
-    // glCreateShader return 0 when there is an error 
-    GLuint shader = glCreateShader(shaderType);
-    if(!shader) { return error; }
+  // glCreateShader return 0 when there is an error
+  GLuint shader = glCreateShader(shaderType);
+  if (!shader) { return error; }
 
-    glShaderSource(shader, 1, &shaderSource, NULL);
-    glCompileShader(shader);
+  glShaderSource(shader, 1, &shaderSource, NULL);
+  glCompileShader(shader);
 
-    if(isCompilationOk(shader)) { return shader; }
+  if (isCompilationOk(shader)) { return shader; }
 
-    showShaderInfoLog(shader);
-    glDeleteShader(shader);
+  showShaderInfoLog(shader);
+  glDeleteShader(shader);
 
-    return error;
+  return error;
 }
