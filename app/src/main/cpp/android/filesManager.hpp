840b8b2e7dd9ef8378cab9b51986d75bbfac89bc
simonppg <simonppg@gmail.com>
2021-09-30 00:07:41 -0700

    Fix android build

--- app/src/main/cpp/android/filesManager.hpp
+++ app/src/main/cpp/android/filesManager.hpp
@@ -1,13 +1,9 @@
-//
-// Created by simonppg on 28/06/18.
-//
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifndef FILES_MANAGER_HPP
-#define FILES_MANAGER_HPP
+#ifndef DEPRECATED_ANDROID_FILES_MANAGER_HPP
+#define DEPRECATED_ANDROID_FILES_MANAGER_HPP
 
 #include <android/asset_manager.h>
 
@@ -16,7 +12,7 @@ extern AAssetManager *g_pAssetManager;
 void load_asset_manager(AAssetManager *);
 char* load_file(const char *filePath);
 
-#endif // FILES_MANAGER_HPP
+#endif // DEPRECATED_ANDROID_FILES_MANAGER_HPP
 
 #ifdef __cplusplus
 }
