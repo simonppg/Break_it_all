d229bb074af924b6b16c53dd9eb9256620071ff2
simonppg <simonppg@gmail.com>
2021-10-01 22:20:06 -0700

    Fix the tests

--- app/src/main/cpp/tests/sqrt_test.cpp
+++ app/src/main/cpp/tests/sqrt_test.cpp
@@ -12,7 +12,7 @@ TEST(SquareRootTest, PositiveNos)
 TEST(SquareRootTest, NegativeNos)
 {
     ASSERT_EQ(-1.0, squareRoot(-15.0));
-    ASSERT_EQ(-1.0, squareRoot(-0.2+1));
+    ASSERT_EQ(-1.0, squareRoot(-0.2));
 }
 
 // int main(int argc, char **argv)
