aafe0dacb21eb23ac1df528f31de644b368156e2
simonppg <simonppg@gmail.com>
2021-10-02 11:55:05 -0700

    Change tabs to spaces

--- app/src/main/java/com/example/game/GameActivity.java
+++ app/src/main/java/com/example/game/GameActivity.java
@@ -13,131 +13,131 @@ import javax.microedition.khronos.opengles.GL10;
 
 public final class GameActivity extends Activity {
 
-	/**
-	 * GLSurfaceView.
-	 */
-	private GLSurfaceView mView;
+  /**
+   * GLSurfaceView.
+   */
+  private GLSurfaceView mView;
 
-	@Override
-	protected void onCreate(final Bundle icicle) {
-		super.onCreate(icicle);
-		Bundle extras = getIntent().getExtras();
+  @Override
+  protected void onCreate(final Bundle icicle) {
+    super.onCreate(icicle);
+    Bundle extras = getIntent().getExtras();
 
-		GLSurfaceView.Renderer renderer = new GLSurfaceView.Renderer() {
-			@Override
-			public void onSurfaceCreated(final GL10 gl, final EGLConfig config) {
-				Game.surfaceCreated();
-			}
+    GLSurfaceView.Renderer renderer = new GLSurfaceView.Renderer() {
+      @Override
+      public void onSurfaceCreated(final GL10 gl, final EGLConfig config) {
+        Game.surfaceCreated();
+      }
 
-			@Override
-			public void onSurfaceChanged(final GL10 gl, final int width, final int height) {
-				Game.surfaceChanged(width, height);
-			}
+      @Override
+      public void onSurfaceChanged(final GL10 gl, final int width, final int height) {
+        Game.surfaceChanged(width, height);
+      }
 
-			@Override
-			public void onDrawFrame(final GL10 gl) {
-				Game.drawFrame();
-			}
-		};
+      @Override
+      public void onDrawFrame(final GL10 gl) {
+        Game.drawFrame();
+      }
+    };
 
-		if (extras != null) {
-			final int position = extras.getInt("position");
-			Toast.makeText(getApplicationContext(), "Click ListItem Number " + position,
-					Toast.LENGTH_SHORT).show();
+    if (extras != null) {
+      final int position = extras.getInt("position");
+      Toast.makeText(getApplicationContext(), "Click ListItem Number " + position,
+          Toast.LENGTH_SHORT).show();
 
-			mView = new GLSurfaceView(getApplication());
+      mView = new GLSurfaceView(getApplication());
 
-			mView.setEGLContextClientVersion(2);
-			mView.setRenderer(renderer);
-			mView.queueEvent(new Runnable() {
-				@Override
-				public void run() {
-					Game.init(position, getAssets());
-					Game.notifyCreate();
-				}
-			});
+      mView.setEGLContextClientVersion(2);
+      mView.setRenderer(renderer);
+      mView.queueEvent(new Runnable() {
+        @Override
+        public void run() {
+          Game.init(position, getAssets());
+          Game.notifyCreate();
+        }
+      });
 
-			setContentView(mView);
-		}
-	}
+      setContentView(mView);
+    }
+  }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.notifyStart();
-			}
-		});
-	}
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.notifyStart();
+      }
+    });
+  }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mView.onResume();
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.notifyResume();
-			}
-		});
-	}
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mView.onResume();
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.notifyResume();
+      }
+    });
+  }
 
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.notifyRestart();
-			}
-		});
-	}
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.notifyRestart();
+      }
+    });
+  }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mView.onPause();
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.notifyPause();
-			}
-		});
-	}
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mView.onPause();
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.notifyPause();
+      }
+    });
+  }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.notifyStop();
-			}
-		});
-	}
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.notifyStop();
+      }
+    });
+  }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.notifyDestroy();
-			}
-		});
-	}
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.notifyDestroy();
+      }
+    });
+  }
 
-	@Override
-	public boolean onTouchEvent(final MotionEvent e) {
-		mView.queueEvent(new Runnable() {
-			@Override
-			public void run() {
-				Game.on_touch_event(e.getX(), e.getY());
-			}
-		});
+  @Override
+  public boolean onTouchEvent(final MotionEvent e) {
+    mView.queueEvent(new Runnable() {
+      @Override
+      public void run() {
+        Game.on_touch_event(e.getX(), e.getY());
+      }
+    });
 
-		return super.onTouchEvent(e);
-	}
+    return super.onTouchEvent(e);
+  }
 }
