b1d883a5953f69a0a1cb876ba0c335c40efaaa51
simonppg <simonppg@gmail.com>
2021-10-02 12:05:37 -0700

    Fix format

--- app/src/main/java/com/example/game/package-info.java
+++ app/src/main/java/com/example/game/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Android project entry point is here in MainScreen.java
- * and all the Activities
+ * Project entry point is here in MainScreen.java and all the Activities.
  *
  */
 package com.example.game;
