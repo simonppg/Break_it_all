5e50a616b64d0305e694810f6e4645840323f9ae
simonppg <simonppg@gmail.com>
2021-10-04 13:03:59 -0700

    Use 2 spaces for indentation

--- app/src/main/cpp/androidTarget.cmake
+++ app/src/main/cpp/androidTarget.cmake
@@ -5,26 +5,26 @@ add_subdirectory(shared/strings)
 add_subdirectory(common)
 
 add_library(Platform
-    STATIC
-    android/AndroidFilesManager.cpp
-    android/AndroidPlatform.cpp
-    android/AndroidLogger.cpp)
+  STATIC
+  android/AndroidFilesManager.cpp
+  android/AndroidPlatform.cpp
+  android/AndroidLogger.cpp)
 
 target_link_libraries(Platform
-    StringFormatter
-    -landroid
-    -llog)
+  StringFormatter
+  -landroid
+  -llog)
 
 add_library(BreakItAll
-    SHARED
-    android/androidLib.cpp)
+  SHARED
+  android/androidLib.cpp)
 
 add_library(Game
-    SHARED
-    android/main.cpp)
+  SHARED
+  android/main.cpp)
 
 target_link_libraries(BreakItAll
-    common)
+  common)
 
 target_link_libraries(Game
-    common)
+  common)
