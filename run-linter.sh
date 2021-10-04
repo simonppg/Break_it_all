f31ac964be66a2a868efd5b613791e2d8f6a71ef
simonppg <simonppg@gmail.com>
2021-10-03 19:29:05 -0700

    Fix linter issues

--- run-linter.sh
+++ run-linter.sh
@@ -4,9 +4,9 @@ set -x # echo commands
 SCRIPT_PATH="${0%/*}"
 
 # No matter where you are, the script always runs in this directory
-cd $SCRIPT_PATH
+cd "$SCRIPT_PATH" || exit
 
 docker run \
     -e RUN_LOCAL=ture \
-    -v $PWD:/tmp/lint \
+    -v "$PWD":/tmp/lint \
     github/super-linter
