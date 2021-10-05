3d9fbe105194fa030c7f9dc8c77c8705ff38624b
simonppg <simonppg@gmail.com>
2021-10-05 13:11:07 -0700

    Add test script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x # echo commands
+
+SCRIPT_PATH="${0%/*}"
+
+# No matter where you are, the script always runs in this directory
+cd $SCRIPT_PATH
+
+BUILD_DIR=test-build
+BUILD_TYPE=Release
+
+# Delete folder if exist
+if [ -d "$BUILD_DIR" ]; then rm -Rf $BUILD_DIR; fi
+
+# Create folder, move inside, build and run
+mkdir -pv $BUILD_DIR && cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE && cmake --build $BUILD_DIR --config $BUILD_TYPE && cd $BUILD_DIR && ctest -C $BUILD_TYPE
