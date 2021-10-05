0be73451fcbe0b85ac495813e66a8899c35494cf
simonppg <simonppg@gmail.com>
2021-10-05 13:43:07 -0700

    Format SandBox with clang-format

--- app/src/main/cpp/common/SandBox.hpp
+++ app/src/main/cpp/common/SandBox.hpp
@@ -1,32 +1,32 @@
-#ifndef COMMON_SANDBOX_HPP
-#define COMMON_SANDBOX_HPP
+// Copyright (c) 2021 Simon Puente
+#ifndef APP_SRC_MAIN_CPP_COMMON_SANDBOX_HPP_
+#define APP_SRC_MAIN_CPP_COMMON_SANDBOX_HPP_
 
-#include "IScene.hpp"
 #include "Camera.hpp"
+#include "IScene.hpp"
+#include "Math.hpp"
 #include "Mesh.hpp"
 #include "Object.hpp"
 #include "ShaderProg.hpp"
-#include "Math.hpp"
 
 class SandBox : public IScene {
 private:
-    Math *math = new Math();
+  Math *math = new Math();
 
 public:
-    SandBox();
-    void surfaceCreated() override;
-    void surfaceChanged(int width, int height) override;
-    void render() override;
-    void pause() override;
-    void resume() override;
-    void update() override;
-    bool events(double xpos, double ypos) override;
+  SandBox();
+  void surfaceCreated() override;
+  void surfaceChanged(int width, int height) override;
+  void render() override;
+  void pause() override;
+  void resume() override;
+  void update() override;
+  bool events(double xpos, double ypos) override;
 
-    Mesh *meshes[1];
-    Object *objects[40];
-    ShaderProg *shaderProgs[1];
-    Renderer *renderer;
+  Mesh *meshes[1];
+  Object *objects[40];
+  ShaderProg *shaderProgs[1];
+  Renderer *renderer;
 };
 
-
-#endif // COMMON_SANDBOX_HPP
+#endif // APP_SRC_MAIN_CPP_COMMON_SANDBOX_HPP_
