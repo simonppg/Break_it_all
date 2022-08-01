206e599c643ebae978b35bd9fdf12ce6cba8910e
simonppg <simonppg@gmail.com>
2022-07-31 18:49:58 -0700

    style(object): run clang-format

--- app/src/main/cpp/common/Object.hpp
+++ app/src/main/cpp/common/Object.hpp
@@ -2,40 +2,40 @@
 #define COMMON_OBJECT_HPP
 
 #include "Camera.hpp"
+#include "DrawContext.hpp"
 #include "Mesh.hpp"
 #include "Point3D.hpp"
-#include "ShaderProg.hpp"
 #include "Renderer.hpp"
-#include "DrawContext.hpp"
+#include "ShaderProg.hpp"
 
 class Object {
 protected:
-    ShaderProg *prog;
-    Renderer *renderer;
-    DrawContext *draw_context;
-    Camera *camera;
-    vec3 rotation;
-    float angle;
-    Mesh *mesh;
-    Point3D position;
-    Point3D size;
+  ShaderProg *prog;
+  Renderer *renderer;
+  DrawContext *draw_context;
+  Camera *camera;
+  vec3 rotation;
+  float angle;
+  Mesh *mesh;
+  Point3D position;
+  Point3D size;
 
 public:
-    Object(Camera *pCamera, ShaderProg *pProg, Mesh *pMesh);
+  Object(Camera *pCamera, ShaderProg *pProg, Mesh *pMesh);
 
-    Point3D getPosition();
+  Point3D getPosition();
 
-    void draw();
-    void updatePosition(Point3D position);
-    void updateSize(Point3D size);
-    void set_rotation_angle(float angle);
-    void animate_x();
-    void animate_y();
-    void animate_z();
+  void draw();
+  void updatePosition(Point3D position);
+  void updateSize(Point3D size);
+  void set_rotation_angle(float angle);
+  void animate_x();
+  void animate_y();
+  void animate_z();
 
-    float velocity;
-    float acceleration = 0;
-    float x_direction = 1;
+  float velocity;
+  float acceleration = 0;
+  float x_direction = 1;
 };
 
 #endif // COMMON_OBJECT_HPP
