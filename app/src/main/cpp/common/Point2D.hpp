0b4450977bf34d83fb3801375b2cc7028536ff26
simonppg <simonppg@gmail.com>
2022-07-31 18:18:07 -0700

    style(point2d): fix linter errors

--- app/src/main/cpp/common/Point2D.hpp
+++ app/src/main/cpp/common/Point2D.hpp
@@ -1,21 +1,23 @@
-#ifndef COMMON_POINT2D_HPP
-#define COMMON_POINT2D_HPP
+// Copyright (c) 2022 Simon Puente
+#ifndef APP_SRC_MAIN_CPP_COMMON_POINT2D_HPP_
+#define APP_SRC_MAIN_CPP_COMMON_POINT2D_HPP_
 
 class Point2D {
 public:
-    Point2D(void);
-    Point2D(float x, float y);
-    Point2D(const Point2D &point);
+  Point2D(void);
+  Point2D(float x, float y);
+  Point2D(const Point2D &point);
 
-    float getX();
-    float getY();
+  float getX();
+  float getY();
 
-    Point2D incrementX(float delta);
-    Point2D decrementX(float delta);
-    Point2D incrementY(float delta);
-    Point2D decrementY(float delta);
+  Point2D incrementX(float delta);
+  Point2D decrementX(float delta);
+  Point2D incrementY(float delta);
+  Point2D decrementY(float delta);
+
 private:
-    float x, y;
+  float x, y;
 };
 
-#endif // COMMON_POINT2D_HPP
+#endif // APP_SRC_MAIN_CPP_COMMON_POINT2D_HPP_
