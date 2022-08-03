d064bac7f0f95ae40bc756e3a2db8321d196f1ec
simonppg <simonppg@gmail.com>
2022-08-02 22:05:51 -0700

    fix(gameloop): better game loop

--- app/src/main/cpp/linux/App.cpp
+++ app/src/main/cpp/linux/App.cpp
@@ -2,6 +2,8 @@
 #include "App.hpp"
 
 #include <cstdlib>
+#include <ctime>
+#include <chrono>
 #include <map>
 
 #include "../common/EventFactory.hpp"
@@ -12,6 +14,13 @@
 #include "GLFWKeyMapper.hpp"
 #include "LinuxPlatform.hpp"
 #include "windowmanager/WindowManager.hpp"
+
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::duration;
+using std::chrono::system_clock;
+using std::chrono::high_resolution_clock;
+using std::milli;
 
 App::App() {
   windowManager = new WindowManager(this);
@@ -66,13 +75,50 @@ void App::start(int sceneNumber) {
 
   game = new Game(sceneNumber, platform);
 
+  float MAX_FRAMES_PER_SECOND = 60; // FPS
+  float MAX_UPDATES_PER_SECOND = 60; // UPS
+
+  double fOPTIMAL_TIME = 1'000'000'000 / MAX_FRAMES_PER_SECOND;
+  double uOPTIMAL_TIME = 1'000'000'000 / MAX_UPDATES_PER_SECOND;
+
+  double uDeltaTime = 0, fDeltaTime = 0;
+  int frames = 0, updates = 0;
+  auto startTime = high_resolution_clock::now();
+  auto timer = high_resolution_clock::now();
+
   game->surfaceCreated();
 
   while (!windowManager->shouldClose()) {
+    auto currentTime = high_resolution_clock::now();
+    auto deltaTime = (currentTime - startTime).count();
+    uDeltaTime += deltaTime;
+    fDeltaTime += deltaTime;
+
     windowManager->pollEvents();
 
-    game->update();
-    game->render();
+    if(uDeltaTime >= uOPTIMAL_TIME) {
+      game->update();
+      updates++;
+      uDeltaTime -= uOPTIMAL_TIME;
+    }
+
+    if(fDeltaTime >= fOPTIMAL_TIME) {
+      game->render();
+      frames++;
+      fDeltaTime -= fOPTIMAL_TIME;
+    }
+
+    auto timer2 = high_resolution_clock::now();
+    double elapsed_time_ms = duration<double, milli>(timer2 - timer).count();
+
+    if (elapsed_time_ms >= 1'000) {
+      logger->logi("UPS: %d, FPS: %d", updates, frames);
+
+      updates = 0;
+      frames = 0;
+      elapsed_time_ms = 0;
+      timer =timer2;
+    }
 
     windowManager->refreshWindow();
   }
