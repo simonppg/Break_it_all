5bdf11eb4ff98089864bb22c3e7f618e72730a96
simonppg <simonppg@gmail.com>
2022-08-13 16:39:21 -0700

    style(cpplint): fix cpplint error

--- app/src/main/cpp/common/opengl/ShaderLoader.hpp
+++ app/src/main/cpp/common/opengl/ShaderLoader.hpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2022 Simon Puente
-#ifndef APP_SRC_MAIN_CPP_COMMON_SHADERLOADER_HPP_
-#define APP_SRC_MAIN_CPP_COMMON_SHADERLOADER_HPP_
+#ifndef APP_SRC_MAIN_CPP_COMMON_OPENGL_SHADERLOADER_HPP_
+#define APP_SRC_MAIN_CPP_COMMON_OPENGL_SHADERLOADER_HPP_
 
 #include <GLES3/gl3.h>
 
@@ -15,4 +15,4 @@ public:
   GLuint loadShader(GLenum shaderType, const char *shaderSource);
 };
 
-#endif // APP_SRC_MAIN_CPP_COMMON_SHADERLOADER_HPP_
+#endif // APP_SRC_MAIN_CPP_COMMON_OPENGL_SHADERLOADER_HPP_
