ae0e3395a9c883c373a169af934cd1229376a3f0
simonppg <simonppg@gmail.com>
2023-02-26 23:06:38 -0700

    refactor(assets): use inline to initialize static strings

--- app/src/main/cpp/common/Assets.hpp
+++ app/src/main/cpp/common/Assets.hpp
@@ -8,14 +8,14 @@ using std::string;
 
 class Assets {
 public:
-  static const string TRIANGLE_VERT;
-  static const string TRIANGLE_FRAG;
-  static const string SIMPLE_VERT;
-  static const string SIMPLE_FRAG;
-  static const string CIRCLE_VERT;
-  static const string CIRCLE_FRAG;
-  static const string CUBE_VERT;
-  static const string CUBE_FRAG;
+  inline static const string TRIANGLE_VERT = "examples/triangle/triangle.vert";
+  inline static const string TRIANGLE_FRAG = "examples/triangle/triangle.frag";
+  inline static const string SIMPLE_VERT = "simple.vert";
+  inline static const string SIMPLE_FRAG = "simple.frag";
+  inline static const string CIRCLE_VERT = "circle.vert";
+  inline static const string CIRCLE_FRAG = "circle.frag";
+  inline static const string CUBE_VERT = "cube/cube.vert";
+  inline static const string CUBE_FRAG = "cube/cube.frag";
 };
 
 #endif // APP_SRC_MAIN_CPP_COMMON_ASSETS_HPP_
