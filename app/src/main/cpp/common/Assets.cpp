454be03e4fbba36d64cc708d3f5f320667317a1c
simonppg <simonppg@gmail.com>
2023-02-26 23:00:44 -0700

    refactor(assets): use string instead of C strings

---
+++ app/src/main/cpp/common/Assets.cpp
@@ -0,0 +1,10 @@
+#include "Assets.hpp"
+
+const string Assets::TRIANGLE_VERT = "examples/triangle/triangle.vert";
+const string Assets::TRIANGLE_FRAG = "examples/triangle/triangle.frag";
+const string Assets::SIMPLE_VERT = "simple.vert";
+const string Assets::SIMPLE_FRAG = "simple.frag";
+const string Assets::CIRCLE_VERT = "circle.vert";
+const string Assets::CIRCLE_FRAG = "circle.frag";
+const string Assets::CUBE_VERT = "cube/cube.vert";
+const string Assets::CUBE_FRAG = "cube/cube.frag";
