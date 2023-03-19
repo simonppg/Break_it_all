76fdadaf7c2b3b139ff1f609c6370790039d966a
simonppg <simonppg@gmail.com>
2023-03-19 11:59:43 -0700

    build(cmake): rename libraries

--- app/src/main/cpp/androidTarget.cmake
+++ app/src/main/cpp/androidTarget.cmake
@@ -6,14 +6,14 @@ add_subdirectory(shared/events)
 add_subdirectory(shared/platform)
 add_subdirectory(common)
 
-add_library(Platform
+add_library(AndroidPlatform
   STATIC
   android/AndroidFilesManager.cpp
   android/AndroidPlatform.cpp
   android/AndroidLogger.cpp)
 
-target_link_libraries(Platform
-  PlatformBase
+target_link_libraries(AndroidPlatform
+  Platform
   StringFormatter
   -landroid
   -llog)
@@ -27,7 +27,9 @@ add_library(Game
   android/main.cpp)
 
 target_link_libraries(BreakItAll
-  common)
+  common
+  AndroidPlatform)
 
 target_link_libraries(Game
-  common)
+  common
+  AndroidPlatform)
