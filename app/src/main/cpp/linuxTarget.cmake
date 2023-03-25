7457cb4f42d192242ab91a57890b1f9cacbfabb6
simonppg <simonppg@gmail.com>
2023-03-25 16:59:59 -0700

    build(cmake): organize cmake files

--- app/src/main/cpp/linuxTarget.cmake
+++ app/src/main/cpp/linuxTarget.cmake
@@ -10,45 +10,5 @@ add_subdirectory(platform)
 add_subdirectory(glwrapper)
 add_subdirectory(game)
 add_subdirectory(linuxapp/windowmanager)
-
-add_library(LinuxPlatform
-  STATIC
-  linuxapp/LinuxFilesManager.cpp
-  linuxapp/LinuxPlatform.cpp
-  linuxapp/LinuxLogger.cpp)
-
-target_link_libraries(LinuxPlatform
-  Platform
-  StringFormatter
-  WindowManager)
-
-add_executable(linuxVersion
-  linuxapp/linuxBin.cpp)
-
-add_executable(linuxLauncher
-  linuxapp/GLFWKeyMapper.cpp
-  linuxapp/App.cpp
-  linuxapp/main.cpp)
-
-target_link_libraries(linuxVersion
-  common
-  LinuxPlatform)
-
-target_link_libraries(linuxLauncher
-  common
-  LinuxPlatform)
-
-set_target_properties(linuxLauncher linuxVersion
-  PROPERTIES
-  RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}")
-
-add_custom_target(runNew
-  COMMAND linuxVersion
-  DEPENDS linuxVersion
-  WORKING_DIRECTORY ${CMAKE_PROJECT_DIR})
-
-add_custom_target(run
-  COMMAND linuxLauncher ${CMD_ARGS}
-  DEPENDS linuxLauncher
-  WORKING_DIRECTORY ${CMAKE_PROJECT_DIR})
+add_subdirectory(linuxapp)
 
