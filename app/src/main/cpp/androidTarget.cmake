24f90e21d1d8fbcf16420637d228cbcd6e8d8e0c
simonppg <simonppg@gmail.com>
2023-03-25 17:33:55 -0700

    build(cmake): organize libs

--- app/src/main/cpp/androidTarget.cmake
+++ app/src/main/cpp/androidTarget.cmake
@@ -7,30 +7,3 @@ add_subdirectory(platform)
 add_subdirectory(glwrapper)
 add_subdirectory(game)
 
-add_library(AndroidPlatform
-  STATIC
-  androidlib/AndroidFilesManager.cpp
-  androidlib/AndroidPlatform.cpp
-  androidlib/AndroidLogger.cpp)
-
-target_link_libraries(AndroidPlatform
-  Platform
-  StringFormatter
-  -landroid
-  -llog)
-
-add_library(BreakItAll
-  SHARED
-  androidlib/androidLib.cpp)
-
-add_library(Game
-  SHARED
-  androidlib/main.cpp)
-
-target_link_libraries(BreakItAll
-  common
-  AndroidPlatform)
-
-target_link_libraries(Game
-  common
-  AndroidPlatform)
